b8a1f7fa7e4e9d7687ca396695938dbadbe7f648
Jerry <one_way@aliyun.com>
2017-07-28 13:48:04 +0800

    add readme

--- src/test/java/jedis/JedisDemo1.java
+++ src/test/java/jedis/JedisDemo1.java
@@ -2,6 +2,8 @@ package jedis;
 
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  *测试
@@ -19,5 +21,31 @@ public class JedisDemo1 {
         String value=jedis.get("name");
         System.out.println(value);
         jedis.close();
+    }
+
+    /**
+     * 连接池测试
+     */
+    @Test
+    public void demo2(){
+        JedisPoolConfig config=new JedisPoolConfig();
+        config.setMaxTotal(30);
+        config.setMaxIdle(10);
+
+        JedisPool pool=new JedisPool(config,"127.0.0.1",6379);
+
+        Jedis jedis=null;
+        try {
+            jedis=pool.getResource();
+            jedis.set("pool","JedisPool");
+            String value=jedis.get("pool");
+            System.out.println(value);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (jedis!=null)
+            jedis.close();
+            pool.close();
+        }
     }
 }
